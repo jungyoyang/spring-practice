ba999397ed1e304cbbaa0396ede0316f3abc9962
Jungyo Yang(Window) <2jungyo@naver.com>
2026-01-23 12:38:27 +0900

    member domian execution and test  (1.20)

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,4 +1,20 @@
 package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
+
+	//구현체가 없이 만들면 NPE로 터지기 때문에 new를 통해 구현체 생성
+	private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+	//회원가입
+	@Override
+	public void join(Member member){
+		memberRepository.save(member);
+	}
+
+	//회원조회
+	@Override
+	public Member findMember(Long memberId){
+		return memberRepository.findById(memberId);
+	}
 }
+
