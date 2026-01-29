54d5bb11aeb7a81a0acf6eaceaf386db30ba5dd0
Jungyoyang <2jungyo@naver.com>
2026-01-29 17:55:24 +0900

    AppConfig(1.29)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,13 +1,23 @@
 package hello.core;
 
+import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
 
+//조립담당자이자, 공연기획자
 public class AppConfig {
 	public MemberService memberService(){
+		//TODO 파라미터에서 new를 하는게 생성자주입?
 		return new MemberServiceImpl(new MemoryMemberRepository());
 	}
+
+	public OrderService orderService() {
+		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+	}
+
 
 
 }
