df597bd56e5fd696e3c9d947388cb174ca623344
Jungyo Yang(Window) <2jungyo@naver.com>
2026-02-02 12:33:20 +0900

    DI(2.2)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -10,14 +10,10 @@ import hello.core.order.OrderServiceImpl;
 //조립담당자이자, 공연기획자
 public class AppConfig {
 	public MemberService memberService(){
-		//TODO 파라미터에서 new를 하는게 생성자주입?
 		return new MemberServiceImpl(new MemoryMemberRepository());
 	}
 
 	public OrderService orderService() {
 		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
 	}
-
-
-
 }
