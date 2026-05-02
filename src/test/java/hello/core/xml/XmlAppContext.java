cbcaf0bfca17fd9790c10a0d5e9446ee80f4a133
Jungyo Yang(Window) <2jungyo@naver.com>
2026-05-02 22:13:18 +0900

    AutoAppConfig(5.2)

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -11,10 +11,8 @@ import static org.assertj.core.api.Assertions.*;
 public class XmlAppContext {
 	@Test
 	void xmlAppContext() {
-		ApplicationContext ac = new
-			GenericXmlApplicationContext("appConfig.xml");
-		MemberService memberService = ac.getBean("memberService",
-			MemberService.class);
+		ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+		MemberService memberService = ac.getBean("memberService", MemberService.class);
 		assertThat(memberService).isInstanceOf(MemberService.class);
 	}
 }
