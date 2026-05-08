22f806709ae98cbe5583e57932af77bf76039184
Jungyo Yang(Window) <2jungyo@naver.com>
2026-05-08 20:31:02 +0900

    Overriding(5.8)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,14 +1,19 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 import static org.springframework.context.annotation.ComponentScan.*;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
 @Configuration
 @ComponentScan(
 	excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
 		Configuration.class))
 public class AutoAppConfig {
+
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,15 +1,21 @@
 package hello.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-
+@Component
 public class OrderServiceImpl implements OrderService {
 
 	private final MemberRepository memberRepository;
 	private final DiscountPolicy discountPolicy;
 
+	@Autowired
 	public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+		System.out.println("memberRepository = " + memberRepository);
+		System.out.println("discountPolicy = " + discountPolicy);
 		this.memberRepository = memberRepository;
 		this.discountPolicy = discountPolicy;
 	}
